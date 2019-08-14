1d92abcb5e4b8b055114e1b2d4c04479b0c1cca1
Alexey Nagornov <al.nagornov@gmail.com>
2019-08-14 11:48:41 +0400

    fix link styles

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule
   ]
 })
 export class HomeModule { }

1256bfa09ee8c70b81d968e6361b809f928f1e35
Alexey Nagornov <al.nagornov@gmail.com>
2019-08-15 12:31:15 +0400

    404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './modules/home/home/home.component';
+import { PageNotFoundComponent } from './modules/shared/components/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
@@ -12,11 +13,14 @@ const routes: Routes = [
     path: 'shipments',
     loadChildren: () => import('./modules/shipments/shipments.module').then(mod => mod.ShipmentsModule)
   },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NzButtonModule, NzCardModule, NzIconModule, NzInputModule, NzSpinModule, NzTableModule } from 'ng-zorro-antd';
+import { NzButtonModule, NzCardModule, NzDropDownModule, NzIconModule, NzInputModule, NzSpinModule, NzTableModule } from 'ng-zorro-antd';
 import { LoaderComponent } from './components/loader/loader.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RouterModule } from '@angular/router';
 
 const MODULES = [
   CommonModule,
+  RouterModule,
   FormsModule,
   ReactiveFormsModule,
   NzButtonModule,
@@ -13,11 +16,13 @@ const MODULES = [
   NzInputModule,
   NzSpinModule,
   NzIconModule,
-  NzTableModule
+  NzTableModule,
+  NzDropDownModule
 ];
 
 const DECLARATIONS = [
-  LoaderComponent
+  LoaderComponent,
+  PageNotFoundComponent
 ];
 
 @NgModule({
